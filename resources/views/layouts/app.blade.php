8495420050012c362b6d80f7446bc20db6691bac
Sarath Kumar K <kumar.sarath588@gmail.com>
2016-04-26 19:53:21 +0530

    Updated calm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Calm List
+                    Calm Is Awesome
                 </a>
             </div>
 
