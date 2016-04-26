3309dfe4805c3e10a1ae169abca8c88e753939c2
Sarath Kumar K <kumar.sarath588@gmail.com>
2016-04-26 23:01:46 +0530

    update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Calm Is Awesome and again Awesome and again
+                    Calm Is Awesome and again Awesome!
                 </a>
             </div>
 
